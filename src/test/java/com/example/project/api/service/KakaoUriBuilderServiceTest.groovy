fafc0ce1b8d6e5db54b09f2ed7035e9f2590a81d
SEVIN <1213hope@gmail.com>
2024-03-04 22:37:39 +0900

    Add Spock 테스트코드추가

---
+++ src/test/java/com/example/project/api/service/KakaoUriBuilderServiceTest.groovy
@@ -0,0 +1,27 @@
+package com.example.project.api.service
+
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+
+class KakaoUriBuilderServiceTest extends Specification {
+
+    private KakaoUriBuilderService kakaoUriBuilderService
+
+    def setup() {
+        kakaoUriBuilderService = new KakaoUriBuilderService()
+    }
+
+    def "buildUriByAddressSearch - 한글 파라미터 인코딩 테스트"() {
+        given:
+        String address = "서울 중랑구"
+        def charset = StandardCharsets.UTF_8
+
+        when:
+        def uri = kakaoUriBuilderService.buildUriByAddressSearch(address)
+        def decodedResult = URLDecoder.decode(uri.toString(), charset)
+
+        then:
+        decodedResult == "https://dapi.kakao.com/v2/local/search/address.json?query=서울 성북구"
+    }
+}
